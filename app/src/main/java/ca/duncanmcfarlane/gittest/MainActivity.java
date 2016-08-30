bf670d5c2cd90abd5ecf1500fc8c21838c159f5e
Duncan McFarlane <duncan.m.mcfarlane@gmail.com>
2016-08-30 06:02:14 -0400

    Added code and class

--- app/src/main/java/ca/duncanmcfarlane/gittest/MainActivity.java
+++ app/src/main/java/ca/duncanmcfarlane/gittest/MainActivity.java
@@ -1,15 +1,40 @@
 package ca.duncanmcfarlane.gittest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> daysArray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        daysArray.add("Sunday");
+        daysArray.add("Monday");
+        daysArray.add("Tuesday");
+        daysArray.add("Wednesday");
+        daysArray.add("Thursday");
+        daysArray.add("Friday");
+        daysArray.add("Saturday");
     }
 
     // TODO: 2016-08-30 "need to add some code here"
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        for (String day : daysArray) {
+            Toast.makeText(MainActivity.this, "The day is " + day, Toast.LENGTH_SHORT).show();
+            for (int i = 0; i < 5000; i++) {
+                int count =+ i;
+            }
+        }
+    }
 }
---
+++ app/src/main/java/ca/duncanmcfarlane/gittest/NewClass.java
@@ -0,0 +1,19 @@
+package ca.duncanmcfarlane.gittest;
+
+import java.util.ArrayList;
+
+public class NewClass {
+    private ArrayList<String> arrayList = new ArrayList<>();
+
+    public NewClass(int intValue, String strValue) {
+        arrayList.set(intValue, strValue);
+    }
+
+    public ArrayList<String> getArrayList() {
+        return arrayList;
+    }
+
+    public void setArrayList(ArrayList<String> arrayList) {
+        this.arrayList = arrayList;
+    }
+}
